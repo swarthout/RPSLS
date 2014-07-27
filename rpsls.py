aa7a9a509d4ea6833921221c33943a5dd1a28d5d
scott-swarthout <scott.swarthout724@gmail.com>
2014-07-27 13:20:56 +0000

    added win/loss record and better input handling

--- rpsls.py
+++ rpsls.py
@@ -11,26 +11,54 @@ rules = [['paper','covers','rock'],
          ['lizard','poisions','spock'],
          ['rock','crushes','lizard'],
          ['rock','crushes','scissors']]
+validpicks = ['rock','paper','scissors','lizard','spock']
 #This is the class where the hand and the win status are stored 
 #
 class player():
     win = False
+    hand = ""
+    win_count = 0
+    lose_count = 0
+    tie_count = 0
     
     def makechoice(self,choice):
         self.hand = choice
     def youwin(self):
         self.win = True
-    
+        self.win_count +=1
+    def youlose(self):
+        self.win = False
+        self.lose_count +=1
+    def youtie(self):
+        self.win = False
+        self.tie_count+=1
+    def reset(self):
+        self.hand = ""
+        self.win = False
+    def showrecord(self):
+        print("Your record: %d-%d-%d"%(self.win_count,self.lose_count,self.tie_count))
 human = player()
 computer = player()
 
+
 def gameloop():
+    
+    
+    computerpick = validpicks[random.randrange(5)] #This will return either RPSLS
 
-    aipick = rules[random.randrange(0,10,2)][0] #This will return either RPSLS
+    computer.makechoice(computerpick) 
+    while human.hand == "": #This will loop until the player puts in a valid input
+        humanpick = input("3,2,1... Rock! Paper! Scissors! Lizard! Spock! Pick one: ").lower()
+        if humanpick in validpicks:
+            human.makechoice(humanpick)
+    	
+    
+        else:
+            print('Not a valid input! Try again...')
+                        
+                        
 
-    computer.makechoice(aipick) 
-
-    human.makechoice(input("3,2,1... Rock! Paper! Scissors! Lizard! Spock! Pick one: ").lower())
+    
 
 
 
@@ -38,13 +66,17 @@ def gameloop():
 
     if human.hand == computer.hand:
         print("You tie!")
+        human.youtie()
+        computer.youtie()
     else:
         def findwinner(player1,player2):
             for x in range(len(rules)):
                 if rules[x][0] == player1.hand and rules[x][2] == player2.hand:
                     player1.youwin()
+                    player2.youlose()
                     return 0
             player2.youwin()
+            player1.youlose()
             return 0
         findwinner(human,computer)
 
@@ -58,13 +90,13 @@ def gameloop():
                 if rules[x][2] == human.hand and rules[x][0] == computer.hand:
                     print(" ".join(rules[x]))
             print("You lose!")
+    human.showrecord()
     playagain = input("Do you want to play again? (Y/N): ").lower()
 
     if playagain == 'y':
+        human.reset()
+        computer.reset()
         gameloop()
 gameloop()
-
-
-
 
 
