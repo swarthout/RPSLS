fa906f59a9c466fb565c2a783e0265f007650109
scott-swarthout <scott.swarthout724@gmail.com>
2014-07-28 21:25:18 +0000

    Added learning AI and difficulty selector

--- rpsls.py
+++ rpsls.py
@@ -22,6 +22,7 @@ class player():
     tie_count = 0
     total_games = 0
     win_percentage = 0.00
+    history = []
     def getpercentage(self):
         self.total_games = self.win_count+self.lose_count+self.tie_count
         if self.total_games !=0:
@@ -40,6 +41,7 @@ class player():
         self.win = False
         self.tie_count+=1
     def reset(self):
+        
         self.hand = ""
         self.win = False
     def showrecord(self):
@@ -47,22 +49,52 @@ class player():
 human = player()
 computer = player()
 
+difficulty = input("Welcome to RSPLS! Do you want to play against: \n\n1.) A random guesser\n2.) A mildly intelligent AI\nEnter 1 or 2: ")
+while difficulty != '1' and difficulty != '2':
+    difficulty= input("Not a valid input! Enter difficulty level (1 or 2): ")
 
 def gameloop():
     
+    def most_common(lst):
+        max = 0
+        maxitemlist = []
+        for x in set(lst):
+            count =  lst.count(x)
+            if count >= max:
+                max = count
+                maxitemlist.append(x)
+        return maxitemlist
+    favoritepicks = most_common(human.history)
+    counterpicks = []
+    for favorite in favoritepicks:
+        for rule in rules:
+            if rule[2] == favorite:
+                counterpicks.append(rule[0])
     
-    computerpick = validpicks[random.randrange(5)] #This will return either RPSLS
-
-    computer.makechoice(computerpick) 
+    randompick = validpicks[random.randrange(len(validpicks))] #This will return either RPSLS
+    if len(counterpicks)!=0:
+        learnedpick = counterpicks[random.randrange(len(counterpicks))]
+    else:
+        learnedpick = randompick
+    
+    
+    
+    if difficulty == "1":
+        computer.makechoice(randompick)
+    if difficulty == "2":
+        computer.makechoice(learnedpick)
+            
     while human.hand == "": #This will loop until the player puts in a valid input
         humanpick = input("3,2,1... Rock! Paper! Scissors! Lizard! Spock! Pick one: ").lower()
         if humanpick in validpicks:
             human.makechoice(humanpick)
+            human.history.append(humanpick)
     	
     
         else:
             print('Not a valid input! Try again...')
                         
+	 
 
     print("You picked: %s \nComputer picked: %s" %(human.hand,computer.hand))
 
@@ -94,16 +126,20 @@ def gameloop():
             print("You lose!")
     human.getpercentage()
     human.showrecord()
+    human.reset()
+    computer.reset()
+    
+    
     playagain = input("Do you want to play again? (Y/N): ").lower()
+    
 
     if playagain == 'y':
-        human.reset()
-        computer.reset()
+        
         gameloop()
     else:
         print(("Games Played: %d "%human.total_games))
         human.showrecord()
+        print("Goodbye!\n")
         quit()
 gameloop()
-
 
