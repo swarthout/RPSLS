61c671a5bd370435fea5fb9d1b9e4c084ab918bd
scott-swarthout <scott.swarthout724@gmail.com>
2014-07-31 15:03:46 +0000

    added custom ai based on research done by chinese academy of sciences

--- rpsls.py
+++ rpsls.py
@@ -13,9 +13,10 @@ rules = [['paper','covers','rock'],
          ['rock','crushes','scissors']]
 validpicks = ['rock','paper','scissors','lizard','spock']
 #This is the class where the hand and the win status are stored 
-#
+
 class player():
     win = False
+    lastwin = False
     hand = ""
     win_count = 0
     lose_count = 0
@@ -23,7 +24,7 @@ class player():
     total_games = 0
     win_percentage = 0.00
     history = []
-    def getpercentage(self):
+    def getpercentage(self): #calculates your win percentage
         self.total_games = self.win_count+self.lose_count+self.tie_count
         if self.total_games !=0:
             self.win_percentage  = (self.win_count/(self.total_games))*100
@@ -43,15 +44,21 @@ class player():
     def reset(self):
         
         self.hand = ""
+        if self.win == False:
+            self.lastwin = False
+        
+        else:
+            self.lastwin = True
+            
         self.win = False
     def showrecord(self):
         print("Your record: %d-%d-%d \nWin Percentage: %.2f"%(self.win_count,self.lose_count,self.tie_count,self.win_percentage))
 human = player()
 computer = player()
 
-difficulty = input("Welcome to RSPLS! Do you want to play against: \n\n1.) A random guesser\n2.) A mildly intelligent AI\nEnter 1 or 2: ")
-while difficulty != '1' and difficulty != '2':
-    difficulty= input("Not a valid input! Enter difficulty level (1 or 2): ")
+difficulty = input("Welcome to RSPLS! Do you want to play against: \n\n1.) A random guesser\n2.) A mildly intelligent AI\n3.) An experimental expert AI\nEnter 1, 2, or 3: ")
+while difficulty != '1' and difficulty != '2' and difficulty != '3':
+    difficulty= input("Not a valid input! Enter difficulty level (1, 2, or 3): ")
 
 def gameloop():
     
@@ -66,10 +73,19 @@ def gameloop():
         return maxitemlist
     favoritepicks = most_common(human.history)
     counterpicks = []
-    for favorite in favoritepicks:
-        for rule in rules:
-            if rule[2] == favorite:
-                counterpicks.append(rule[0])
+    
+    def findopposites(list_of_options): #Input list of picks, returns a list of picks that will beat them.
+        opposite_list = []
+        for option in list_of_options:
+            for rule in rules:
+                if rule[2] == option:
+                    opposite_list.append(rule[0])
+        return(opposite_list)
+    counterpicks = findopposites(favoritepicks)
+    
+    
+    
+        
     
     randompick = validpicks[random.randrange(len(validpicks))] #This will return either RPSLS
     if len(counterpicks)!=0:
@@ -77,20 +93,32 @@ def gameloop():
     else:
         learnedpick = randompick
     
+    if len(human.history) != 0:
+        if computer.lastwin == True:
+            smartpick = human.history[-1]
+        if computer.lastwin == False:
+            smartpick = findopposites([human.history[-1]])[random.randrange(2)]
+    else:
+        smartpick = randompick
+    
     
     
     if difficulty == "1":
         computer.makechoice(randompick)
+        computer.history.append(randompick)
     if difficulty == "2":
         computer.makechoice(learnedpick)
+        computer.history.append(learnedpick)
+    if difficulty == "3":
+        computer.makechoice(smartpick)
+        computer.history.append(smartpick)
             
     while human.hand == "": #This will loop until the player puts in a valid input
         humanpick = input("3,2,1... Rock! Paper! Scissors! Lizard! Spock! Pick one: ").lower()
         if humanpick in validpicks:
             human.makechoice(humanpick)
             human.history.append(humanpick)
-    	
-    
+    	    
         else:
             print('Not a valid input! Try again...')
                         
